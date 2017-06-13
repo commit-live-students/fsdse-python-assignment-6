bf4d42794307a75c482a5b3876cd9dbd18c48147
coded9 <coded9@users.noreply.github.com>
2017-06-13 04:15:22 +0000

    Done

---
+++ account.py
@@ -0,0 +1,20 @@
+class Account:
+    balance = 0
+
+class Savings(Account):
+    def __init__(self,name):
+        self.name = name
+    def deposit(self,bal):
+        self.balance += bal
+    def withdraw(self,bal):
+        if self.balance > bal:
+            self.balance -= bal
+class Current(Account):
+    def __init__(self,name):
+        self.name = name
+    def deposit(self,bal):
+        if bal > 15000:
+            self.balance += bal
+    def withdraw(self,bal):
+        if self.balance > bal:
+            self.balance = self.balance - bal - bal*0.1
