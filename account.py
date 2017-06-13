8dfd2447222e621e8dfe459b88626bf3b7ac638e
coded9 <coded9@users.noreply.github.com>
2017-06-13 04:19:28 +0000

    Done

--- account.py
+++ account.py
@@ -1,14 +1,14 @@
 class Account:
     balance = 0
-
-class Savings(Account):
-    def __init__(self,name):
-        self.name = name
     def deposit(self,bal):
         self.balance += bal
     def withdraw(self,bal):
         if self.balance > bal:
             self.balance -= bal
+class Savings(Account):
+    def __init__(self,name):
+        self.name = name
+
 class Current(Account):
     def __init__(self,name):
         self.name = name
